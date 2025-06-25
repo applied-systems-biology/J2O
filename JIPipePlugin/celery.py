24cfb119bf20862bd8ac3edca35909e92ceec3e0
MariusWank <mariuswank@web.de>
2025-06-25 16:25:59 +0200

    Updated celery.py for better overview

--- JIPipePlugin/celery.py
+++ JIPipePlugin/celery.py
@@ -1,30 +1,23 @@
 import os
 import logging
 from celery import Celery
-from omero.config import ConfigXml
-import json
-
-# Locate the grid config.xml from your OMERO installation
-cfg_file = os.path.join(os.environ["OMERODIR"], "etc", "grid", "config.xml")
-cfg = ConfigXml(cfg_file, read_only=True)
-raw = cfg.as_map().get("omero.web.caches")
-caches = json.loads(raw)
-redis_backend = caches["default"]["LOCATION"]
+from django.conf import settings
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JIPipePlugin.settings')
 
-app = Celery('JIPipePlugin', broker=redis_backend, backend=redis_backend)
+try:
+    cache_location = settings.CACHES["default"]["LOCATION"]
+except Exception as e:
+    raise RuntimeError("omero.web.caches not correctly defined!")
+
+app = Celery('JIPipePlugin', broker=cache_location, backend=cache_location)
 # run on omero-web: celery -A JIPipePlugin worker --loglevel=info -E
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
 logger = logging.getLogger(__name__)
-
-print(f"Using broker: {app.conf.broker_url}")
-print(f"Using backend: {app.conf.result_backend}")
-
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
