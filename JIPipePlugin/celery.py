19f6f32eb1ff8d82af25d566eabd5627e3cd5d94
MariusWank <mariuswank@web.de>
2025-07-09 10:29:20 +0200

    Changed celery.py to get CACHE location from env var.

--- JIPipePlugin/celery.py
+++ JIPipePlugin/celery.py
@@ -2,22 +2,48 @@ import os
 import logging
 from celery import Celery
 from django.conf import settings
+import json
+
+logger = logging.getLogger(__name__)
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JIPipePlugin.settings')
 
 try:
-    cache_location = settings.CACHES["default"]["LOCATION"]
-except Exception as e:
-    raise RuntimeError("omero.web.caches not correctly defined!")
+    cache_settings_raw = os.getenv("OMERO_CONFIG_CACHES")
+    
+    if cache_settings_raw is None:
+        raise ValueError("Environment variable OMERO_CONFIG_CACHES is not set.")
+    
+    try:
+        cache_settings = json.loads(cache_settings_raw)
+    except json.JSONDecodeError:
+        raise ValueError("OMERO_CONFIG_CACHES does not contain valid JSON.")
 
-app = Celery('JIPipePlugin', broker=cache_location, backend=cache_location)
+    # Now check for required fields
+    if "default" not in cache_settings:
+        raise ValueError("OMERO_CONFIG_CACHES is missing 'default' cache config.")
+
+    default_cache = cache_settings["default"]
+    default_cache_location = default_cache.get("LOCATION")
+    default_cache_backend = default_cache.get("BACKEND")
+
+    if not default_cache_location or not default_cache_backend:
+        raise ValueError("Default cache config must include both 'LOCATION' and 'BACKEND'.")
+
+    if "redis" not in default_cache_location or "redis" not in default_cache_backend:
+        raise ValueError("Default cache location and/or backend is not redis! "
+                         "Check that OMERO_CONFIG_CACHES is set correctly and omero.web.caches is using redis.")
+
+except ValueError as error:
+    logger.error(f"Cache configuration error: {error}")
+    raise
+
+app = Celery('JIPipePlugin', broker=default_cache_location, backend=default_cache_location)
 # run on omero-web: celery -A JIPipePlugin worker --loglevel=info -E
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
-
-logger = logging.getLogger(__name__)
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
